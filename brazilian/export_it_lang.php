36add486b89cf994c0d94c08f88823df5cd9da15
MarchiMedia <marchimedia@gmail.com>
2012-11-07 16:33:56 -0300

    Brazilian Portuguese Added
    Brazilian Portuguese Added (pt-br) in the brazilian folder

---
+++ brazilian/export_it_lang.php
@@ -0,0 +1,79 @@
+<?php
+$lang = array(
+
+// Necessário para a página MÓDULOS
+
+'export_it_module_name'		=> 'Export It',
+'export_it_module_description'	=> 'Permite que os dados do ExpressionEngine sejam exportados em vários formatos.',
+
+//----------------------------------------
+
+// Additional Key => Value pairs go here
+'main' => 'Principal',
+'license_number' => 'Número da Licença',
+'missing_license_number' => 'Por favor insira seu número de licença. <a href="#config_url#">Insira sua licença</a> ou Compre uma Licença <a href="http://devot-ee.com/add-ons/export-it/">Devot:ee</a> ou <a href="http://cartthrob.com/cart/products#third_party_export_it">CartThrob.com</a>',
+'channel_entries' => 'Registro dos Canais',
+'comments' => 'Comentários',
+'mailing_list' => 'Lista de E-mails',
+'export' => 'Exportar',
+'configure_api' => 'Configurar API',
+'configure_cp_export' => 'Configure a Exportação',
+'member_group' => 'Grupo de Membros',
+'export_format' => 'Exportar no Formato',
+'include_custom_fields' => 'Incluir Campos Customizados',
+'basic_member_fields' => 'Campos Básicos de Membro',
+'exclude_duplicates' => 'Excluir Duplicatas',
+'date_range' => 'Período da Data',
+'past_day' => 'Ontem',
+'past_week' => 'Semana Anterior',
+'past_month' => 'Mês Anterior',
+'past_six_months' => 'Seis Meses Anteriores',
+'past_year' => 'Ano Passado',
+'open' => 'Aberto',
+'closed' => 'Fechado',
+'complete_select' => 'Seleção Completa',
+'enable_api' => 'Ativar API',
+'enable_api_instructions' => 'A API tem que ser ativada manualmente pela segurança. Uma vez que ela estiver ativada, você poderá efetuar as requisições via API.',
+'api_key' => 'Chave API',
+'api_key_instructions' => 'Uma Chave API é necessária para segurança. Qualquer solicitação para sua API sem a chave API será rejeitada. Ela poderá ser uma string randômica desde que não tenha mais de 32 caracteres. ',
+
+'api_disabled' => 'Api Desativada!',
+'bad_method' => 'Método Ruim',
+'api_url' => 'URL da API',
+'api_url_instructions' => 'Esta é a URL utilizada para acessar a API. Tenha certeza de ler a <a href="http://mithra62.com/docs/view/export-it-rest-server-api">documentação</a> antes de trabalhar com a API.',
+
+'screen_name' => 'Nome de Tela',
+'email' => 'E-mail',
+'join_date' => 'Data de Ingresso',
+'any_date' => 'Qualquer Data',
+'keywords' => 'Palavras-chave',
+		
+'emails' => 'E-mails',
+'mailinglists' => 'Listas de Mail',
+'comments' => 'Comentários',
+'comment' => 'Comentário',
+'select_channel' => 'Selecionar Canal',	
+'entry_title' => 'Título',	
+		
+'no_matching_comments' => 'Nenhum Comentário Localizado',
+'no_matching_members' => 'Nenhum Membro Localizado',
+'start_date' => 'Data de Início',
+'end_date' => 'Data Final',
+'no_matching_channel_entries' => 'Nenhum Registro de Canal Localizado',
+'channel_title' => 'Titulo do Canal',
+		
+'members_list_limit' => 'Limite da Lista de Membros',
+'members_list_limit_instructions' => 'Quantos mais itens você deseja listar por página na página de Exportação da Lista de Membro?',
+'channel_entries_list_limit' => 'Limite da Lista de Registros de Canal',
+'channel_entries_list_limit_instructions' => 'Quantos itens a mais você deseja listar por página no Limite da Exportação da Lista de Registros de Canal?',
+'comments_list_limit' => 'Limite da Lista Comentários',
+'comments_list_limit_instructions' => 'Quantos itens a mais você deseja listar por página na página Exportar Lista de Comentários?',
+'mailing_list_limit' => 'Limita da Lista de Mail',
+'mailing_list_limit_instructions' => 'Quantos itens você deseja listar por página na página Exportar Lista de Mail?',
+'export_it_date_format' => 'Formato da Data',
+'export_it_date_format_instructions' => 'O formato da data você deseja que o Export It use ao exportar os dados. Note que o formato esteja em conformidade com o <a href="http://expressionengine.com/user_guide/templates/date_variable_formatting.html#date-formatting-codes" target="_blank">o formato de data do ExpressionEngine</a>.',
+'members_list_limit' => 'Limitar Lista de Membros',
+		
+'global_config' => 'Configuração Global',
+''=>''
+);
